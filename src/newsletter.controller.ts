fe824eb208102e2416bb03ba21a4b78b1c9287c1
Marc Stammerjohann <marcstammerjohann@web.de>
2020-02-16 00:21:31 +0100

    feat(newsletter): signup for newsletter

--- src/newsletter.controller.ts
+++ src/newsletter.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body, ConflictException } from '@nestjs/common';
 import { AppService } from './app.service';
 import { PrismaService } from './services/prisma.service';
 import { Newsletter } from './newsletter';
+import { NewsletterSignupDto } from './newsletter.dto';
 
 @Controller()
 export class NewsletterController {
@@ -15,8 +16,28 @@ export class NewsletterController {
     return this.appService.getHello();
   }
 
-  @Get('newsletter')
-  async newsletter(): Promise<Newsletter[]> {
-    return this.prisma.newsletter.findMany();
+  // @Get('newsletter')
+  // async newsletter(): Promise<Newsletter[]> {
+  //   return this.prisma.newsletter.findMany();
+  // }
+
+  @Post('newsletter')
+  async newsletterSignup(
+    @Body() { email }: NewsletterSignupDto,
+  ): Promise<Newsletter> {
+    let signup = await this.prisma.newsletter.findOne({ where: { email } });
+
+    if (signup) {
+      signup = await this.prisma.newsletter.update({
+        where: { email },
+        data: { enabled: true },
+      });
+    } else {
+      signup = await this.prisma.newsletter.create({
+        data: { email },
+      });
+    }
+
+    return signup;
   }
 }
---
+++ src/newsletter.dto.ts
@@ -0,0 +1,3 @@
+export class NewsletterSignupDto {
+  email: string;
+}
