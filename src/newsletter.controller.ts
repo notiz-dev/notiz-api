554cd6116c9714b9e5c146719ff27a98422e2b61
Marc Stammerjohann <marcstammerjohann@web.de>
2020-02-17 19:22:35 +0100

    feat(subscribe): subscribe, confirm and unsubscribe

--- src/newsletter.controller.ts
+++ src/newsletter.controller.ts
@@ -1,7 +1,12 @@
-import { Controller, Get, Post, Body, NotFoundException } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  BadRequestException,
+} from '@nestjs/common';
 import { AppService } from './app.service';
 import { PrismaService } from './services/prisma.service';
-import { Newsletter } from './newsletter';
 import { SubscribeDto } from './subscribe.dto';
 
 @Controller()
@@ -13,55 +18,48 @@ export class NewsletterController {
 
   @Get()
   getHello(): string {
-    return this.appService.getHello();
-  }
-
-  @Get('subscribe')
-  async newsletter(): Promise<Newsletter[]> {
-    return this.prisma.newsletter.findMany();
+    return 'notiz.dev';
   }
 
   @Post('subscribe')
-  async subscribe(@Body() { email }: SubscribeDto): Promise<Newsletter> {
-    let subscribption = await this.prisma.newsletter.findOne({
+  async subscribe(@Body() { email }: SubscribeDto) {
+    const subscription = await this.prisma.newsletter.findOne({
       where: { email },
     });
 
-    if (subscribption) {
-      subscribption = await this.prisma.newsletter.update({
+    if (subscription) {
+      await this.prisma.newsletter.update({
         where: { email },
         data: { subscribed: true },
       });
     } else {
-      subscribption = await this.prisma.newsletter.create({
+      await this.prisma.newsletter.create({
         data: { email },
       });
     }
-
-    return subscribption;
   }
 
   @Post('confirm')
   async confirm(@Body() { email }: SubscribeDto) {
-    await this.prisma.newsletter.update({
-      where: { email },
-      data: { subscribed: false },
-    });
+    try {
+      await this.prisma.newsletter.update({
+        where: { email },
+        data: { confirmed: true },
+      });
+    } catch (error) {
+      throw new BadRequestException();
+    }
   }
 
   @Post('unsubscribe')
   async unsubscribe(@Body() { email }: SubscribeDto) {
-    // const subscribption = await this.prisma.newsletter.findOne({
-    //   where: { email },
-    // });
-
-    // if (subscribption) {
-    await this.prisma.newsletter.update({
-      where: { email },
-      data: { subscribed: false },
-    });
-    // } else {
-    //   throw new NotFoundException();
-    // }
+    try {
+      await this.prisma.newsletter.update({
+        where: { email },
+        data: { subscribed: false },
+      });
+    } catch (error) {
+      throw new BadRequestException();
+    }
   }
 }
