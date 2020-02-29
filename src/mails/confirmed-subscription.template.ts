e075dd3c963d81c9fedf365dfeefa83df659201f
Marc Stammerjohann <marcstammerjohann@web.de>
2020-02-29 18:10:18 +0100

    feat(mails): update templates

--- src/mails/confirmed-subscription.template.ts
+++ src/mails/confirmed-subscription.template.ts
@@ -1,10 +1,10 @@
 export const confirmedSubscriptionTemplate = (uuid: string) => {
   return (
     '<p>Hello,</p>' +
-    '<p>Your subscription to our list has been confirmed.</p>' +
-    '<p>Thank you for subscribing!</p>' +
-    '<p>Cheers</p>' +
-    '<p><a href="https://notiz.dev">notiz.dev</a> Team</p>' +
+    '<p>Your subscription to our list has been confirmed.<br>' +
+    'Thank you for subscribing! We will keep you updated about our latest posts.</p>' +
+    '<p>Cheers<br>' +
+    '<a href="https://notiz.dev">notiz.dev</a> Team</<a>' +
     `<p><a href="https://notiz.dev/unsubscribe?uuid=${uuid}">Unsubscribe</a></p>`
   );
 };
--- src/mails/subscription.template.ts
+++ src/mails/subscription.template.ts
@@ -2,11 +2,11 @@ export const subscriptionTemplate = (uuid: string) => {
   return (
     '<p>Hello,</p>' +
     '<p>thank you for taking an interest in <a href="https://notiz.dev">notiz.dev</a>.</p>' +
-    '<p>Do you mind confirming your email for us to make sure it was really you who signed up?</p>' +
-    `<p><a href="https://notiz.dev/confirm-subscription?uuid=${uuid}">https://notiz.dev/confirm-subscription?uuid=${uuid}</a></p>` +
+    '<p>Do you mind confirming your email for us to make sure it was really you who signed up?<br>' +
+    `<a href="https://notiz.dev/confirm-subscription?uuid=${uuid}">https://notiz.dev/confirm-subscription?uuid=${uuid}</a></<a>` +
     "<p>If you received this email by mistake, simply delete it. You won't be subscribed if you don't click the confirmation link above.</p>" +
-    '<p>Cheers</p>' +
-    '<p><a href="https://notiz.dev">notiz.dev</a> Team</p>' +
+    '<p>Cheers<br>' +
+    '<a href="https://notiz.dev">notiz.dev</a> Team</>' +
     `<p><a href="https://notiz.dev/unsubscribe?uuid=${uuid}">Unsubscribe</a></p>`
   );
 };
