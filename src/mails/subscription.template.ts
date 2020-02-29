fd04b647cadf353eb5c33d36750385fa2d20e420
Marc Stammerjohann <marcstammerjohann@web.de>
2020-02-29 14:20:21 +0100

    feat(subscribe): add website link

--- src/mails/subscription.template.ts
+++ src/mails/subscription.template.ts
@@ -1,7 +1,7 @@
 export const subscriptionTemplate = (uuid: string) => {
   return (
     '<p>Hello there,</p>' +
-    '<p>thanks for taking an interest in <strong>notiz.dev</strong>.</p>' +
+    '<p>thanks for taking an interest in <a href="https://notiz.dev">notiz.dev</a>.</p>' +
     `<p>Please confirm your email for us: <a href="https://notiz.dev/confirm-subscription?uuid=${uuid}">https://notiz.dev/confirm-subscription?uuid=${uuid}</a></p>` +
     '<p>Cheers</p>' +
     '<p><strong>notiz.dev</strong> team</p>'
