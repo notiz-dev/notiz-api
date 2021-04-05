d9f12afcdd84375e19c597896c7dba9da2399890
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2021-04-05 20:26:22 +0200

    add personal access token auth

--- src/github/github.service.ts
+++ src/github/github.service.ts
@@ -1,6 +1,6 @@
+import { ConfigService } from '@nestjs/config';
 import { GitHubRepo } from './entities/github-repo.entity';
 import {
-  HttpException,
   Injectable,
   NotFoundException,
   BadRequestException,
@@ -11,14 +11,15 @@ import { Octokit } from '@octokit/rest';
 export class GithubService {
   private octokit: Octokit;
 
-  constructor() {
-    // TODO add authentication https://octokit.github.io/rest.js/v18#authentication
-    this.octokit = new Octokit();
+  constructor(private config: ConfigService) {
+    // using personal access token - https://octokit.github.io/rest.js/v18#authentication
+    this.octokit = new Octokit({ auth: config.get('GITHUB_TOKEN') });
   }
 
   async repo(owner: string, repo: string): Promise<GitHubRepo> {
     try {
-      return (await this.octokit.rest.repos.get({ owner, repo })).data;
+      const response = await this.octokit.rest.repos.get({ owner, repo });
+      return response.data;
     } catch (e) {
       if (e.status === 404) {
         throw new NotFoundException(`Repo ${owner}/${repo} not found`);
