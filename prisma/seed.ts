24bc3e6fec64311c2aebab4293452203063c905a
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2021-10-04 20:24:13 +0200

    update deps

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,14 +1,12 @@
 import { PrismaClient } from '@prisma/client';
-import * as dotenv from 'dotenv';
 
 const prisma = new PrismaClient();
 
 async function main() {
-  dotenv.config();
   console.log('Seeding...');
 
   await prisma.newsletter.create({
-    data: { email: 'me@marcjulian.de' },
+    data: { email: 'hi@notiz.dev' },
   });
 }
 
