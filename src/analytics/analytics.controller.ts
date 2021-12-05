95125293518c87cd73725452525bb4bcc16fb239
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2021-12-05 03:53:58 +0100

    add latest posts endpoint

--- src/analytics/analytics.controller.ts
+++ src/analytics/analytics.controller.ts
@@ -1,13 +1,18 @@
+import { HttpService } from '@nestjs/axios';
 import { Controller, DefaultValuePipe, Get, Query } from '@nestjs/common';
 import { ApiQuery, ApiTags } from '@nestjs/swagger';
-import { Observable } from 'rxjs';
+import { map, Observable, tap, filter } from 'rxjs';
 import { PageRequest, Period } from './entities/analytics.entity';
 import { AnalyticsService } from './analytics.service';
+import { Post } from './entities/post.entity';
 
 @Controller('analytics')
 @ApiTags('Analytics')
 export class AnalyticsController {
-  constructor(private readonly analyticsService: AnalyticsService) {}
+  constructor(
+    private readonly analyticsService: AnalyticsService,
+    private httpService: HttpService,
+  ) {}
 
   @Get('')
   @ApiQuery({ name: 'period', required: false, enum: Period })
@@ -15,5 +20,20 @@ export class AnalyticsController {
     @Query('period', new DefaultValuePipe(Period['30d'])) period: Period,
   ): Observable<PageRequest[]> {
     return this.analyticsService.topPages(period);
+  }
+
+  @Get('latest')
+  latestPosts() {
+    return this.httpService
+      .get('https://notiz.dev/assets/scully-routes.json')
+      .pipe(
+        map((response) => response.data as Post[]),
+        map((pages) => pages.filter((p) => p.route.startsWith('/blog/'))),
+        map((pages) =>
+          pages.sort((p1, p2) =>
+            new Date(p1.publishedAt) > new Date(p2.publishedAt) ? -1 : 1,
+          ),
+        ),
+      );
   }
 }
---
+++ src/analytics/entities/post.entity.ts
@@ -0,0 +1,13 @@
+export class Post {
+  route: string;
+  title: string;
+  description: string;
+  published: boolean;
+  publishedAt: string;
+  updatedAt: string;
+  tags: string[];
+  keywords: string[];
+  authors: string[];
+  github: string;
+  sourceFile: string;
+}
